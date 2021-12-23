8b5855bf1d80fe4e9c6fe96f3be7430cd8497625
GRENADEable <kahtim.dar@gmail.com>
2021-12-23 22:28:24 +0500

    - Setting up buttons in the main menu - Organised folder - Imported text mesh pro

---
+++ Sharkbait/Assets/Scripts/GameManagerMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerMenu : MonoBehaviour
+{
+    #region Public Variables
+
+    #endregion
+
+    #region Private Variables
+
+    #endregion
+
+    #region Unity Callbacks
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region My Functions
+
+    #endregion
+}
