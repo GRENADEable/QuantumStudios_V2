949c78c375265ced10aa76f182cf1123c1d68059
GRENADEable <kahtim.dar@gmail.com>
2021-12-23 09:14:56 +0500

    Intialised project

---
+++ Sharkbait/Assets/Scripts/CameraFollowSinglePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowSinglePlayer : MonoBehaviour
+{
+
+    #region Private Variables
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Vector3 Offset;
+    #endregion
+    void Start()
+    {
+        if (player != null)
+            Offset = transform.position - player.transform.position;
+    }
+    void LateUpdate()
+    {
+        if (player != null)
+            transform.position = player.transform.position + Offset;
+    }
+}
---
+++ Sharkbait/Assets/Scripts/RandomWhirlpoolSinglePlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWhirlpoolSinglePlayer : MonoBehaviour 
+{
+	public float maxSize;
+	public float growInt;
+	private float waitTime;
+	
+	void Start () 
+	{
+		StartCoroutine(Scale());
+		waitTime = Random.Range(1f,3f);
+	}
+
+	IEnumerator Scale()
+	{
+		float timer = 0;
+
+		while(true)
+		{
+			//Scale all the axis to have the same value to work with float instead of comparing via vectors
+			while(maxSize > transform.localScale.x)
+			{
+				timer += Time.deltaTime;
+				transform.localScale += new Vector3(1f,1f,0.04683586f) * Time.deltaTime * growInt * Random.Range(1f, 10f);
+				yield return null;
+			}
+			
+			//Reset the timer
+			yield return new WaitForSeconds(waitTime);
+
+			timer = 0;
+			while(1 < transform.localScale.x)
+			{
+				timer += Time.deltaTime;
+				transform.localScale -= new Vector3(1f,1f,0.04683586f) * Time.deltaTime * growInt * Random.Range(1f, 10f);
+				yield return null;
+			}
+
+			timer = 0;
+			yield return new WaitForSeconds(waitTime);
+		}
+	}
+	void Update()
+	{
+		
+	}
+	
+}
